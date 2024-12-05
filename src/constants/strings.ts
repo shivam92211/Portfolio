7c596ade74752c01fff8199818e48c9a7ec87d87
Shivam Vishwakarma <shivam92211@gmail.com>
2024-12-05 14:53:10 +0530

    add the portfolio

---
+++ src/constants/strings.ts
@@ -0,0 +1,38 @@
+class Strings {
+  static telegram = "Telegram";
+  static telegramLink = "https://telegram.me/nixrajput";
+  static telegramUsername = "nixrajput";
+
+  static github = "GitHub";
+  static githubLink = "https://www.github.com/shivam92211";
+  static githubUsername = "Shivam92211";
+
+  static linkedIn = "LinkedIn";
+  static linkedInLink = "https://www.linkedin.com/in/shivam92211";
+  static linkedInUsername = "shivam92211";
+
+  static instagram = "Instagram";
+  static instagramLink = "https://www.instagram.com/nixrajput";
+  static instagramUsername = "nixrajput";
+
+  static twitter = "Twitter";
+  static twitterLink = "https://www.twitter.com/Shivam_92211";
+  static twitterUsername = "Shivam_92211";
+
+  static email = "Email";
+  static primaryEmailLink = "mailto:shivm92211@gmail.com";
+  static primaryEmail = "shivm92211@gmail.com";
+
+  static buyMeACoffee = "BuyMeACoffee";
+  static buyMeACoffeeLink = "https://buymeacoffee.com/nixrajput";
+  static buyMeACoffeeUsername = "nixrajput";
+
+  static koFi = "Ko-Fi";
+  static koFiLink = "https://ko-fi.com/nixrajput";
+  static koFiUsername = "nixrajput";
+
+  static fullName = "Nikhil Rajput";
+  static shortName = "nixrajput";
+}
+
+export default Strings;
